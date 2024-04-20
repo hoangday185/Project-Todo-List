5b209da8cd1381ef581e79d94501bc6a115d7dcf
jarvietnguyen318@gmail.com <jarvietnguyen318@gmail.com>
2024-04-20 19:56:34 +0700

    feature edit todo

--- src/Components/TaskInput/TaskInput.tsx
+++ src/Components/TaskInput/TaskInput.tsx
@@ -1,32 +1,52 @@
+import { Todo } from '@/@types/todo.type'
 import styles from '@/Components/TaskInput/taskInput.module.scss'
 import { useState } from 'react'
 
 interface TaskInputProps {
   addTodo: (name: string) => void
+  currentTodo: Todo | null
+  editTodo: (name: string) => void
+  finishEditTodo: () => void
 }
 
 const TaskInput = (props: TaskInputProps) => {
-  const { addTodo } = props
+  const { addTodo, currentTodo, editTodo, finishEditTodo } = props
 
   const [name, setName] = useState<string>('')
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    //chức năng thêm mới task, thêm xong nhớ xóa input
     event.preventDefault()
-    addTodo(name)
-    setName('')
+    if (currentTodo) {
+      finishEditTodo()
+      if (name) setName('')
+    } else {
+      addTodo(name)
+      setName('')
+    }
   }
 
   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //ko có hàm này là thành uncontrolled component
     const { value } = event.target
-    setName(value)
+    if (currentTodo) {
+      editTodo(value)
+    } else {
+      setName(value)
+    }
   }
 
   return (
     <div className='mb-2'>
       <h1 className={styles.title}>To do list</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input type='text' value={name} placeholder='caption goes here' onChange={onChangeInput} />
-        <button type='submit'>➕</button>
+        <input
+          type='text'
+          placeholder='caption goes here'
+          onChange={onChangeInput}
+          value={currentTodo ? currentTodo.name : name}
+        />
+        <button type='submit'>{currentTodo ? '✔' : '➕'}</button>
       </form>
     </div>
   )
--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -6,11 +6,13 @@ import { useState } from 'react'
 
 const TodoList = () => {
   const [todos, setTodos] = useState<Todo[]>([])
-
+  const [currentTodo, setCurrentTodo] = useState<Todo | null>(null)
   const doneTodos = todos.filter((todo) => todo.done)
   const notdoneTodos = todos.filter((todo) => !todo.done)
   const addTodo = (name: string) => {
+    //ta quản lý todo ở todolist nên hàm addTodo cần được viết ở đây và truyền xuống dưới component input
     const todo: Todo = {
+      //name được lấy ở component input
       name,
       done: false,
       id: new Date().toISOString()
@@ -18,7 +20,27 @@ const TodoList = () => {
     setTodos((prev) => [...prev, todo])
   }
 
+  const startEditTodo = (id: string) => {
+    //do list todo ở đây nên ta phải làm hàm startEditTodo trên này và truyền xuống taskList component
+    //tìm todo bằng id
+    const findTodo = todos.find((todo) => todo.id == id)
+    //set currentTodo
+    if (findTodo) {
+      setCurrentTodo(findTodo)
+    }
+  }
+
+  const editTodo = (name: string) => {
+    //khi edit 1 cái todo thì ta phải set lại list todo
+    //nhưng tag input nên ở component input nên ta sẽ dùng viết hàm trên hàm và truyền xuống
+    setCurrentTodo((prevTodo) => {
+      if (prevTodo) return { ...prevTodo, name }
+      return null
+    })
+  }
+
   const handleDoneTodo = (id: string, done: boolean) => {
+    //viết hàm xử lý checked ở đây để cho component con tái sử dụng
     setTodos((prev) => {
       return prev.map((todo) => {
         if (todo.id == id) {
@@ -29,13 +51,28 @@ const TodoList = () => {
     })
   }
 
+  const finishEditTodo = () => {
+    //hàm này để set lại list todo khi edit xong nhưng nút submit lại ở dưới component taskInput nên viết hàm này truyền xuống để
+    //để xài
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === (currentTodo as Todo).id) {
+          return currentTodo as Todo
+        }
+        return todo
+      })
+    })
+    setCurrentTodo(null)
+  }
+
   console.log(todos)
+
   return (
     <div className={styles.todoList}>
       <div className={styles.container}>
-        <TaskInput addTodo={addTodo} />
-        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} />
-        <TaskList doneTaskList={true} todos={doneTodos} handleDoneTodo={handleDoneTodo} />
+        <TaskInput addTodo={addTodo} currentTodo={currentTodo} editTodo={editTodo} finishEditTodo={finishEditTodo} />
+        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} startEditTodo={startEditTodo} />
+        <TaskList doneTaskList={true} todos={doneTodos} handleDoneTodo={handleDoneTodo} startEditTodo={startEditTodo} />
       </div>
     </div>
   )
